dbff590d5b2ae1945f21c6ac4f7fb3e6657c356d
yaro-here <7213633+yzcode-here@users.noreply.github.com>
2020-07-10 11:06:53 +0300

    Added comments, removed unnecessary select()

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,19 +1,18 @@
-library(tidyverse)
+library(tidyverse) # needed for pipingm map_df and read_csv
 
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-  ids <- sprintf("%03d", id)
-  files <- list.files(path = file.path(getwd(), directory), pattern = paste0(ids, collapse = "|"), full.names = TRUE)
+  ids <- sprintf("%03d", id) # adding leading zeros to ids to match with file names
+  files <- list.files(path = file.path(getwd(), directory), pattern = paste0(ids, collapse = "|"), full.names = TRUE) # creating a vector of full paths to files with requested ids
 
-  value <- pull(
+  value <- pull( # reading in files with specified ids 
     files %>%
-      map_df(~read_csv(.)) %>%
-      select(pollutant) %>%
+      map_df(~read_csv(.)) %>% # combining files in one tibble
       summarise(
-        mean = mean(.data[[pollutant]], na.rm = TRUE)
+        mean = mean(.data[[pollutant]], na.rm = TRUE) # calculating the mean values of the requested pollutant
         )
     )
 
-  print(value)
+  print(value) # printing out the mean value 
 
 }
 
